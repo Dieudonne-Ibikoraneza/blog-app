51a09b05a000d4961d3b86151469c7926a77389e
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-28 09:39:33 +0200

    Finished setting up my tailwindcss configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-
 function App() {
-  return <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
+  return <h1 className="text-3xl font-openSans underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
